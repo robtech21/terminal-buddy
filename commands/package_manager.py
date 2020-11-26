fc1eb3a1b8e9e708802e2e54fbf7f4bce9190c1f
HGE-studio <57325674+HGE-studio@users.noreply.github.com>
2020-11-26 12:09:38 +0100

    Update package_manager.py

--- commands/package_manager.py
+++ commands/package_manager.py
@@ -7,3 +7,13 @@ def install_with_apt(program):
 def search_with_apt(keyword):
     print("Searching...")
     os.system(f"sudo apt search {keyword}")
+
+def update_apt_database():
+    print("Updating package database...")
+    os.system("sudo apt update")
+
+def upgrade_packages():
+    print("Upgrading packages")
+    os.system("sudo apt upgrade")
+
+def
